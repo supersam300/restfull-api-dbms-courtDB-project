436f68a3e8060064917a66f7e7e5b0f6cfa9d98c
Samarth Atreya <atreyasamarth@gmail.com>
2025-04-16 18:24:32 +0530

    code did not save to commit lmao

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql2/promise.js'; //i have created a pool here where the data base is accessed
+
+const pool = mysql.createPool(
+{  
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'CourtSystem',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+
+export default pool;
