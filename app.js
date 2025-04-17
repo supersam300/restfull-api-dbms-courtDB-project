6ed75afd52fbfa8ceffdd99e78127e630a9dcf95
Samarth Atreya <atreyasamarth@gmail.com>
2025-04-17 07:35:45 +0530

    updated port to 8080

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ import cors from 'cors';
 import courtRoutes from './routs/courtroutes.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = 8080;
+app.use(cors({
+  origin: 'http://localhost:8080',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
+}));
 
-app.use(cors());
+
 app.use(express.json());
 
+// ✅ Routes
 app.use('/api/courts', courtRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('Court System Backend Running');
 });
 
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
