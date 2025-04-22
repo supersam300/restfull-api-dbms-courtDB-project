74447face0c9eccb21065ec8cdd8a65a98bea899
Samarth Atreya <atreyasamarth@gmail.com>
2025-04-22 10:19:28 +0530

    updated to use .env

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise.js'; //i have created a pool here where the da
 
 const pool = mysql.createPool(
 {  
-    host:'localhost',
-    user:'root',
-    password:'1234',
-    database:'CourtSystem',
+    host:  process.env.DB_HOST,
+    user:  process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:  process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
